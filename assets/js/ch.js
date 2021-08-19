63b905d69909a0ddaf322f075b74d7a230e3e223
abhijitkumar39 <abhijitkumar39@gmail.com>
2021-08-19 13:53:30 +0530

    Add files via upload

---
+++ assets/js/ch.js
@@ -0,0 +1,18 @@
+{/* <script type="text/javascript" content-type="application/javascript;charset=utf-8" src="/assets/js/typewriter.js"></script> */}
+
+var app = document.getElementById('app');
+							
+var typewriter = new Typewriter(app, {
+					loop: true
+					});
+							
+				typewriter.typeString('Programmer')
+						.pauseFor(1200)
+						.deleteAll()
+						.typeString('Tech Writer')
+						.pauseFor(1200)
+						.deleteAll()
+						.typeString('Time Pass Chef ;)')
+						.pauseFor(1200)
+						.start();
+							
